1c749b58daf23013e8e6a688c1ddcef3d10f5529
truefinch <glushkov.vk@students.dvfu.ru>
2018-06-07 22:52:59 +1000

    Initial commit

---
+++ src/App/Geometry/Line.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: truefinch
+ * Date: 07.06.18
+ * Time: 22:32
+ */
+
+namespace App\Geometry;
+
+
+class Line
+{
+
+}
---
+++ src/App/Geometry/Point.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Geometry;
+
+class Point
+{
+    /**
+     * @var float
+     */
+    private $x;
+
+    /**
+     * @var float
+     */
+    private $y;
+
+    public function __construct(float $x, float $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    /**
+     * @return float
+     */
+    public function getX(): float
+    {
+        return $this->x;
+    }
+
+    /**
+     * @return float
+     */
+    public function getY(): float
+    {
+        return $this->y;
+    }
+
+    /**
+     * @param Point $other
+     * @return float
+     */
+    public function distance(Point $other): float
+    {
+        return sqrt(
+            pow($this->x - $other->getX(), 2) + pow($this->y - $other->getY(), 2)
+        );
+    }
+}
