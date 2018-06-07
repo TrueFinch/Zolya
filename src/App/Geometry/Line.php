e7ff3702b7b039d7472429b17b1cda5baa4427f1
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-06-08 00:13:37 +1000

    Implement some methods in class Line

--- src/App/Geometry/Line.php
+++ src/App/Geometry/Line.php
@@ -8,8 +8,41 @@
 
 namespace App\Geometry;
 
+include 'ShapeInterface.php';
 
-class Line
+class Line implements ShapeInterface
 {
+    /**
+     * @var Point
+     */
+    private $a_, $b_;
 
+    /**
+     * @var string
+     */
+    private $name_;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name_;
+    }
+
+    /**
+     * @return float
+     */
+    public function getArea(): float
+    {
+        return -1;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPerimeter(): float
+    {
+        return $this->a_->distance($this->b_);
+    }
 }
