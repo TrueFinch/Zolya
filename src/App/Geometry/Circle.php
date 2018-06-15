09f04aa262d6b9a68e45004efdc9134cc4573681
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-06-16 07:20:33 +1000

    Rewrite a circle-circle and circle-line intersection, move from class Line realization of circle-line intersection

--- src/App/Geometry/Circle.php
+++ src/App/Geometry/Circle.php
@@ -9,7 +9,7 @@
 namespace App\Geometry;
 
 
-class Circle
+class Circle implements ShapeInterface
 {
     /**
      * Center of the circle
@@ -88,4 +88,159 @@ class Circle
         $this->name_ = $name_;
     }
 
+    /**
+     * @param Point $point
+     * @return bool
+     */
+    public function belong(Point $point): bool
+    {
+        return (pow($point->getX() - $this->center_->getX(), 2) +
+                pow($point->getY() - $this->center_->getY(), 2)) == pow($this->radius_, 2);
+    }
+
+    /**
+     * @return float
+     */
+    public function getArea(): float
+    {
+        return pi() * $this->radius_ * $this->radius_;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPerimeter(): float
+    {
+        return 2 * pi() * $this->radius_;
+    }
+
+    /**
+     * To simplify the task we put that our circle lies in the point (0;0)
+     * and add to result it's real coordinates only at ending
+     * @param float $a_
+     * @param float $b_
+     * @param float $c_
+     * @return array
+     */
+    public function intersectLine(float $a_, float $b_, float $c_): array
+    {
+        /**
+         * @var array Array of Points
+         */
+        $result = array();
+
+        /**
+         * X coordinate of the circle's center
+         * @var float
+         */
+        $p = $this->center_->getX();
+        /**
+         * Y coordinate of the circle's center
+         * @var float
+         */
+        $q = $this->center_->getY();
+        /**
+         * Radius of the circle
+         * @var float
+         */
+        $r = $this->radius_;
+
+
+        /**
+         * Coordinates of nearest point to circle center
+         * @var float
+         * @var float
+         */
+        $x0 = -$a_ * $c_ / ($a_ * $a_ + $b_ * $b_);
+        $y0 = -$b_ * $c_ / ($a_ * $a_ + $b_ * $b_);
+
+        if (abs($c_ * $c_ - $r * $r * ($a_ * $a_ + $b_ * $b_)) == 0) {
+            $p0 = new Point($x0 + $p, $y0 + $q);
+            array_push($result, $p0);
+        } elseif ($c_ * $c_ < $r * $r * ($a_ * $a_ + $b_ * $b_)) {
+            $d = $r * $r - $c_ * $c_ / ($a_ * $a_ + $b_ * $b_);
+            $mult = sqrt($d / ($a_ * $a_ + $b_ * $b_));
+            $p1 = new Point($x0 + $b_ * $mult + $p, $y0 - $a_ * $mult + $q);
+            $p2 = new Point($x0 - $b_ * $mult + $p, $y0 + $a_ * $mult + $q);
+            array_push($result, $p1);
+            array_push($result, $p2);
+        }
+        return $result;
+    }
+
+    /**
+     * @param Rectangle $rect
+     * @return array
+     */
+    public function intersectRect(Rectangle $rect): array
+    {
+        return $rect->intersect($this);
+    }
+
+    public function intersectCircle(Circle $circle): array
+    {
+        /**
+         * @var array Array of Points
+         */
+        $result = array();
+
+        /**
+         * Coordinates x and y and radius of $this circle
+         * @var float
+         * @var float
+         * @var float
+         */
+        $cx1 = $this->center_->getX();
+        $cy1 = $this->center_->getY();
+        $r1 = $this->radius_;
+
+        /**
+         * Coordinates x and y and radius of other circle
+         * @var float
+         * @var float
+         * @var float
+         */
+        $cx2 = $circle->getCenter()->getX() - $cx1;
+        $cy2 = $circle->getCenter()->getY() - $cy1;
+        $r2 = $circle->getRadius();
+
+        if (($cx2 == 0) and ($cy2 == 0)) {
+            if ($r1 == $r2) {
+                $p0 = new Point($cx1 + $r1, $cy1);
+                $p1 = new Point($cx1 - $r1, $cy1);
+                $p2 = new Point($cx1, $cy1 + $r1);
+                array_push($result, $p0);
+                array_push($result, $p1);
+                array_push($result, $p2);
+            }
+        } else {
+            $result = $this->intersectLine(-2 * $cx2, -2 * $cy2, $cx2 + $cy2 - $r2 + $r1);
+        }
+        return $result;
+    }
+
+    /**
+     * @param $shape
+     * @return array
+     */
+    public function intersect($shape): array
+    {
+        /**
+         * @var array of Points
+         */
+        $result = array();
+        switch (gettype($shape)) {
+            case "Line":
+                $p = $shape->getParameters();
+                $result = $this->intersectLine($p[0], $p[1], $p[2]);
+                break;
+            case "Rectangle":
+                $result = $this->intersectRect($shape);
+                break;
+            case "Circle":
+                $result = $this->intersectCircle($shape);
+                break;
+        }
+        return $result;
+    }
 }
