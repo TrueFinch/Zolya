e56c564f53121c4ded7bd0b2f521dfd3bf70d108
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2018-06-23 05:48:37 +1000

    .

---
+++ tests/App/Geometry/RectangleTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: truefinch
+ * Date: 16.06.18
+ * Time: 12:04
+ */
+
+namespace AppTest\App\Geometry;
+
+use App\Geometry\Rectangle;
+use PHPUnit\Framework\TestCase;
+
+class RectangleTest extends TestCase
+{
+
+    public function testGetPc()
+    {
+
+    }
+
+    public function testGetPd()
+    {
+
+    }
+
+    public function testSetPd()
+    {
+
+    }
+
+    public function testBelong()
+    {
+
+    }
+
+    public function testGetArea()
+    {
+
+    }
+
+    public function testSetPa()
+    {
+
+    }
+
+    public function testIntersect()
+    {
+
+    }
+
+    public function testGetPb()
+    {
+
+    }
+
+    public function testSetPc()
+    {
+
+    }
+
+    public function testGetPerimeter()
+    {
+
+    }
+
+    public function testSetPb()
+    {
+
+    }
+
+    public function testGetName()
+    {
+
+    }
+
+    public function testGetPa()
+    {
+
+    }
+
+    public function testSetName()
+    {
+
+    }
+}
