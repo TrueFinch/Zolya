f785a6f559b3daec24a4fa63c3000420e3c8a50f
truefinch <glushkov.vk@students.dvfu.ru>
2018-06-27 06:08:02 +1000

    Begin write implementation of UserRepository. Almost end with findById

---
+++ src/App/Authentication/Repository/UserRepository.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: truefinch
+ * Date: 27.06.18
+ * Time: 4:03
+ */
+
+namespace App\Authentication\Repository;
+
+
+use App\Authentication\UserInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+    /**
+     * MySQL database reference
+     * @var \mysqli
+     */
+    private $db_;
+
+    public function __construct($database)
+    {
+        $this->db_ = $database;
+    }
+
+    /**
+     * Метод ищет пользователя по индентификатору, возвращает UserInterface если пользователь существует, иначе null
+     *
+     * @param int $id
+     * @return UserInterface|null
+     */
+    public function findById(int $id): ?UserInterface
+    {
+        $query = $this->db_->prepare('SELECT * FROM users WHERE id = :id');
+        $query->bind_param(':id', $id);
+        $query->execute();
+        $record = $query->get_result()->fetch_assoc();
+        $query->close();
+        return ($record) ? ($record) : (null);
+        // TODO: Implement findById() method.
+        // TODO: Rewrite return part - we need return UserInterface instead of array.
+    }
+
+    /**
+     * Метод ищет пользователя по login, возвращает UserInterface если пользователь существует, иначе null
+     *
+     * @param string $login
+     * @return UserInterface|null
+     */
+    public function findByLogin(string $login): ?UserInterface
+    {
+        // TODO: Implement findByLogin() method.
+    }
+
+    /**
+     * Метод сохраняет пользоваля в хранилище
+     *
+     * @param UserInterface $user
+     */
+    public function save(UserInterface $user)
+    {
+        // TODO: Implement save() method.
+    }
+}
