4d8b1c6b7a8b383d58c1ea8a4d9c7d77d1a8b552
truefinch <glushkov.vk@students.dvfu.ru>
2018-06-27 04:05:34 +1000

    Create class with utils for geometry classes

--- src/App/Geometry/GeometryUtils.php
+++ src/App/Geometry/GeometryUtils.php
@@ -2,19 +2,24 @@
 /**
  * Created by PhpStorm.
  * User: truefinch
- * Date: 19.06.18
- * Time: 14:57
+ * Date: 27.06.18
+ * Time: 3:35
  */
 
-/**
- *
- * @param float $a
- * @param float $b
- * @param float $c
- * @param float $d
- * @return float
- */
-function det(float $a, float $b, float $c, float $d): float
+namespace App\Geometry;
+
+class GeometryUtils
 {
-    return $a * $d - $b * $c;
+    /**
+     *Determinant of the square matrix 2x2
+     * @param float $a
+     * @param float $b
+     * @param float $c
+     * @param float $d
+     * @return float
+     */
+    public function det(float $a, float $b, float $c, float $d): float
+    {
+        return $a * $d - $b * $c;
+    }
 }
