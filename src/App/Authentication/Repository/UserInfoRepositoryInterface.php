5879715220c9c0e143b747aaa754c6189d005c34
truefinch <glushkov.vk@students.dvfu.ru>
2018-06-27 10:38:37 +1000

    Add class UserInfoRepositoryInterface. It works similarly to UserRepositoryInterface, but gives an access to an information about user, not about his/her login or password

---
+++ src/App/Authentication/Repository/UserInfoRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: truefinch
+ * Date: 27.06.18
+ * Time: 10:30
+ */
+
+namespace App\Authentication\Repository;
+
+
+use App\Authentication\UserInterface;
+
+class UserInfoRepositoryInterface
+{
+}
